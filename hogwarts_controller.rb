5a2053bed54d95f639af41a25465819038e8fca2
smelge <traildogfilms@gmail.com>
2018-10-04 12:04:18 +0100

    Added some views

--- hogwarts_controller.rb
+++ hogwarts_controller.rb
@@ -1,5 +1,10 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require( 'pry-byebug' )
-require_relative( './models/pizza_order' )
+require_relative( './models/hogwarts_stuff.rb' )
 also_reload( './models/*' )
+
+get '/students' do
+  @students = Student.find_all()
+  erb('students')
+end
--- models/hogwarts_stuff.rb
+++ models/hogwarts_stuff.rb
@@ -1,11 +1,10 @@
 require_relative('../db/sql_runner.rb')
 
 class Student
-  attr_reader
-  attr_accessor
+  attr_reader :id,:first_name,:last_name,:house,:age
 
   def initialize(options)
-    @id = options['id']
+    @id = options['id'].to_i
     @first_name = options['first_name']
     @last_name = options['last_name']
     @house = options['house']
@@ -26,5 +25,23 @@ class Student
   def self.delete_all()
     sql = "DELETE FROM students"
     SqlRunner.run(sql)
+  end
+
+  def find_one()
+    sql = "
+      SELECT * FROM students WHERE id = $1
+    "
+    result = SqlRunner.run(sql,[$1])
+    return Student.new(result.first)
+  end
+
+  def self.find_all()
+    sql = "SELECT * FROM students"
+    result = SqlRunner.run(sql)
+    return result.map { |student| Student.new( student ) }
+  end
+
+  def name()
+    return "#{@first_name} #{@last_name}"
   end
 end
